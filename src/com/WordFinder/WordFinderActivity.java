0b5db3125bb7264b2bba1918171558cabd5c76df
Bryan Malyn <bim9262@yahoo.com>
2011-10-28 17:12:11 -0400

    First Commit

---
+++ src/com/WordFinder/WordFinderActivity.java
@@ -0,0 +1,13 @@
+package com.WordFinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WordFinderActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
