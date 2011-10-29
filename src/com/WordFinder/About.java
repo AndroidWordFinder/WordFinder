addb435a526a5df637ddf87992e73303eecd2c86
John Mooring <john@mooring.com>
2011-10-29 03:23:57 -0400

    Added some comments

--- src/com/WordFinder/About.java
+++ src/com/WordFinder/About.java
@@ -3,7 +3,16 @@ package com.WordFinder;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class About extends Activity {
+/*
+ * // -------------------------------------------------------------------------
+/**
+ *  About activity. Shows a simple view crediting the
+ *  authors and describing the project.
+ *
+ *  @author John Mooring (jmooring)
+ *  @version Oct 29, 2011
+ */
+class About extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- src/com/WordFinder/WordFinderActivity.java
+++ src/com/WordFinder/WordFinderActivity.java
@@ -1,29 +1,48 @@
 package com.WordFinder;
 
 import android.content.Intent;
-
 import android.view.Menu;
 import android.view.MenuItem;
-
 import android.app.Activity;
 import android.os.Bundle;
 
+/*
+ * // -------------------------------------------------------------------------
+ /**
+ *  Main application activity.
+ *
+ *  @author John Mooring (jmooring)
+ *  @version Oct 29, 2011
+ */
 public class WordFinderActivity extends Activity {
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.main);
     }
 
+    /**
+     * Constructs the context menu
+     * @returns boolean TODO what does boolean do?
+     */
     public boolean onCreateOptionsMenu(Menu menu) {
-	menu.add(0,0,0,"About");
+	menu.add(0, 0, 0, "About");
 	return true;
     }
 
+    /**
+     * Called when a menu item is selected
+     * @returns boolean TODO what does boolean do?
+     */
     public boolean onOptionsItemSelected(MenuItem i) {
-	startActivity(new Intent(this,About.class));
+	switch (i.getItemId()) {
+	case 0:
+	    startActivity(new Intent(this, About.class));
+	    break;
+	}
 	return true;
     }
 }
-
