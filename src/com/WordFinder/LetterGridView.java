917054ee853e48543c39a93d290ab1fff184c8a0
Bryan Malyn <bim9262@yahoo.com>
2011-11-08 22:48:16 -0500

    Added size

--- src/com/WordFinder/LetterGridView.java
+++ src/com/WordFinder/LetterGridView.java
@@ -9,6 +9,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
+import static com.WordFinder.Tile.State.*;
 
 // -------------------------------------------------------------------------
 /**
@@ -24,6 +25,7 @@ public class LetterGridView
 {
 
     private LetterGrid model;
+
 
 
     // ----------------------------------------------------------
