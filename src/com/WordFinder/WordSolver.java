fe2968c099308f62f9094254ae2b1ec1d4333ee7
Bryan Malyn <bim9262@yahoo.com>
2011-11-26 21:29:12 -0500

    added clearWords() and loadDictionary() to solve() and made those two methods private

--- src/com/WordFinder/WordSolver.java
+++ src/com/WordFinder/WordSolver.java
@@ -10,36 +10,44 @@ import java.util.ArrayList;
  * @author John Mooring (jmooring)
  * @version 2011.11.8
  */
-public class WordSolver {
+public class WordSolver
+{
     private static WordSolver instance;
     private ArrayList<String> foundWords;
-    private Trie dictionary;
-    private final int MAX_DEPTH = 10;
+    private Trie              dictionary;
+    private final int         MAX_DEPTH = 10;
+
 
     /**
      * Singleton constructor
      */
-    private WordSolver() {
-	dictionary = new Trie();
-	foundWords = new ArrayList<String>();
+    private WordSolver()
+    {
+        dictionary = new Trie();
+        foundWords = new ArrayList<String>();
     }
+
 
     /**
      * Clears all found words from the list
      */
-    public void clearWords() {
-	foundWords.clear();
+    private void clearWords()
+    {
+        foundWords.clear();
     }
+
 
     /**
      * Loads the dictionary into a prefix tree
      */
-    public void loadDictionary() {
-	Context c = WordFinderActivity.getInstance(); // will need context to
-						      // load from assets
-	//TODO
-	throw new UnsupportedOperationException("not yet implemented");
+    private void loadDictionary()
+    {
+        Context c = WordFinderActivity.getInstance(); // will need context to
+        // load from assets
+        // TODO
+        throw new UnsupportedOperationException("not yet implemented");
     }
+
 
     /**
      * Adds a single word to the dictionary.
@@ -47,48 +55,68 @@ public class WordSolver {
      * @param word
      *            the word to add to the dictionary
      */
-    public void addWord(String word) {
-	dictionary.add(word);
+    public void addWord(String word)
+    {
+        dictionary.add(word);
     }
+
 
     /**
      * Solves the passed grid
      */
-    public ArrayList<String> solve(LetterGrid grid) {
-	for (int i = 0; i < grid.size(); i++) {
-	    for (int j = 0; j < grid.size(); j++) {
-		solve(grid.getTile(i, j), "",
-			new boolean[grid.size()][grid.size()], MAX_DEPTH);
-	    }
-	}
-	return foundWords;
+    public ArrayList<String> solve(LetterGrid grid)
+    {
+        clearWords();
+        loadDictionary();
+        for (int i = 0; i < grid.size(); i++)
+        {
+            for (int j = 0; j < grid.size(); j++)
+            {
+                solve(
+                    grid.getTile(i, j),
+                    "",
+                    new boolean[grid.size()][grid.size()],
+                    MAX_DEPTH);
+            }
+        }
+        return foundWords;
     }
+
 
     /**
      * Uses recursion to solve the letter grid
      */
-    private void solve(Tile tile, String current, boolean[][] visited, int depth) {
-	String word = current + tile.getLetter();
-	if (dictionary.hasNext(tile.getLetter())) {
-	    dictionary.gotoNext(tile.getLetter());
-	} else {
-	    return;
-	}
-	visited[tile.getX()][tile.getY()] = true;
-	if (dictionary.isWord()) {
-	    foundWords.add(word);
-	}
-	if (depth <= 0) {
-	    return;
-	}
-	for (Tile t : tile.getAdjascent()) {
-	    if (!visited[t.getX()][t.getY()]) {
-		solve(t, word, visited, depth - 1);
-	    }
-	}
-	visited[tile.getX()][tile.getY()] = false;
-	dictionary.pop();
+    private void solve(Tile tile, String current, boolean[][] visited, int depth)
+    {
+        String word = current + tile.getLetter();
+        if (dictionary.hasNext(tile.getLetter()))
+        {
+            dictionary.gotoNext(tile.getLetter());
+        }
+        else
+        {
+            return;
+        }
+        visited[tile.getX()][tile.getY()] = true;
+        if (dictionary.isWord())
+        {
+            foundWords.add(word);
+        }
+        if (depth <= 0)
+        {
+            return;
+        }
+        for (Tile t : tile.getAdjascent())
+        {
+            if (!visited[t.getX()][t.getY()])
+            {
+                solve(t, word, visited, depth - 1);
+            }
+        }
+        visited[tile.getX()][tile.getY()] = false;
+        dictionary.pop();
     }
+
 
     /**
      * Checks to see if the dictionary contains the passed word
@@ -97,25 +125,31 @@ public class WordSolver {
      *            the word to check
      * @return whether or not the word is a word
      */
-    public boolean isWord(String word) {
-	//TODO
-	throw new UnsupportedOperationException("not yet implemented");
+    public boolean isWord(String word)
+    {
+        // TODO
+        throw new UnsupportedOperationException("not yet implemented");
     }
+
 
     /**
      * Returns an arraylist of all words found on the grid
      */
-    public ArrayList<String> getWords() {
-	return foundWords;
+    public ArrayList<String> getWords()
+    {
+        return foundWords;
     }
+
 
     /**
      * Returns an instance of this class
      */
-    public static WordSolver getInstance() {
-	if (instance == null) {
-	    instance = new WordSolver();
-	}
-	return instance;
+    public static WordSolver getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new WordSolver();
+        }
+        return instance;
     }
 }
