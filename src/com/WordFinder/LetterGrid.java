e42a2d9adf2e5271e84a569c24949c64e52e2104
John Mooring <john@mooring.com>
2011-11-28 21:43:25 -0500

    Completed methods in LetterGrid

--- src/com/WordFinder/LetterGrid.java
+++ src/com/WordFinder/LetterGrid.java
@@ -2,166 +2,150 @@ package com.WordFinder;
 
 import java.util.ArrayList;
 import java.util.Observable;
-import java.util.Random;
-import java.util.Vector;
 
-public class LetterGrid
-    extends Observable
-{
-    private Tile[][]        grid;
-    private ArrayList<Tile> path;
+import com.WordFinder.Tile.State;
 
+public class LetterGrid extends Observable {
 
-    /**
-     * Builds a LetterGrid
-     */
-    public LetterGrid()
-    {
-        path = new ArrayList<Tile>();
-    }
+	private Tile[][] grid;
+	private ArrayList<Tile> path;
 
+	/**
+	 * Builds a LetterGrid
+	 */
+	public LetterGrid() {
+		path = new ArrayList<Tile>();
+	}
 
-    /**
-     * Generates and loads a square grid of characters with a side length of the
-     * passed integer
-     *
-     * @param size
-     *            the side length of the square character grid
-     */
-    public void loadRandom(int size)
-    {
-        char[][] lettersGrid = new char[size][size];
-        String letters = "";
-        int length = size * size;
-        int hardLetters = ((int)(Math.random() * length * .1875 + .5));
-        int vowles =
-            (int)(Math.random() * (length * .4375 - length / 4.0 + 1) +
-                length / 4.0);
-        int regLetters = length - vowles - hardLetters;
-        String[] letterTypes = { "bcdfghklmnprstwy", "aeiou", "jqkzxv", };
-        int numOfLetterTypes[] = { regLetters, vowles, hardLetters };
+	/**
+	 * Generates and loads a square grid of characters with a side length of the
+	 * passed integer
+	 * 
+	 * @param size
+	 *            the side length of the square character grid
+	 */
+	public void loadRandom(int size) {
+		char[][] lettersGrid = new char[size][size];
+		String letters = "";
+		int length = size * size;
+		int hardLetters = ((int) (Math.random() * length * .1875 + .5));
+		int vowles = (int) (Math.random() * (length * .4375 - length / 4.0 + 1) + length / 4.0);
+		int regLetters = length - vowles - hardLetters;
+		String[] letterTypes = { "bcdfghklmnprstwy", "aeiou", "jqkzxv", };
+		int numOfLetterTypes[] = { regLetters, vowles, hardLetters };
 
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < numOfLetterTypes[i]; j++)
-            {
-                int insertHere = (int)(letters.length() * Math.random());
-                letters =
-                    letters.substring(0, insertHere)
-                        + letterTypes[i]
-                            .charAt(((int)(letterTypes[i].length() * Math
-                                .random()))) + letters.substring(insertHere);
-            }
-        }
-        int loopCount = 0;
-        for (int i = 0; i < size; i++)
-        {
-            for (int j = 0; j < size; j++)
-            {
-                lettersGrid[i][j] = letters.charAt(loopCount);
-                loopCount++;
-            }
-        }
-        load(lettersGrid);
-    }
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < numOfLetterTypes[i]; j++) {
+				int insertHere = (int) (letters.length() * Math.random());
+				letters = letters.substring(0, insertHere)
+						+ letterTypes[i]
+								.charAt(((int) (letterTypes[i].length() * Math
+										.random())))
+						+ letters.substring(insertHere);
+			}
+		}
+		int loopCount = 0;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				lettersGrid[i][j] = letters.charAt(loopCount);
+				loopCount++;
+			}
+		}
+		load(lettersGrid);
+	}
 
+	/**
+	 * Creates the grid from the passed array of letters. Characters are
+	 * transformed into Tiles, and the tiles are given an array of all adjacent
+	 * tiles.
+	 * 
+	 * @precondition letters is square
+	 * @param letters
+	 *            the letters to load
+	 */
+	public void load(char[][] letters) {
+		this.grid = new Tile[letters.length][letters[0].length];
+		for (int i = 0; i < letters.length; i++) {
+			for (int j = 0; j < letters[i].length; j++) {
+				this.grid[i][j] = new Tile(i, j, letters[i][j]);
+			}
+		}
+		for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[i].length; j++) {
+				for (int di = i - 1; di <= i + 1; di++) {
+					for (int dj = j - 1; dj <= j + 1; dj++) {
+						if (di >= 0 && di < grid.length && dj >= 0
+								&& dj < grid[i].length && !(di == i && dj == j)) {
+							grid[i][j].addAdjascent(grid[di][dj]);
+						}
+					}
+				}
+			}
+		}
+	}
 
-    /**
-     * Creates the grid from the passed array of letters. Characters are
-     * transformed into Tiles, and the tiles are given an array of all adjascent
-     * tiles.
-     */
-    public void load(char[][] letters)
-    {
-        // TODO check to see if letters is square, if it is not, throw an
-        // exception or something like that
-        this.grid = new Tile[letters.length][letters[0].length];
-        for (int i = 0; i < letters.length; i++)
-        {
-            for (int j = 0; j < letters[i].length; j++)
-            {
-                this.grid[i][j] = new Tile(i, j, letters[i][j]);
-            }
-        }
-        for (int i = 0; i < grid.length; i++)
-        {
-            for (int j = 0; j < grid[i].length; j++)
-            {
-                for (int di = i - 1; di <= i + 1; di++)
-                {
-                    for (int dj = j - 1; dj <= j + 1; dj++)
-                    {
-                        if (di >= 0 && di < grid.length && dj >= 0
-                            && dj < grid[i].length && !(di == i && dj == j))
-                        {
-                            grid[i][j].addAdjascent(grid[di][dj]);
-                        }
-                    }
-                }
-            }
-        }
-    }
+	/**
+	 * If the tile is valid, (based on the last selected tile) the tile is added
+	 * to the current path list and has its state changed to DOWN
+	 */
+	public void setSelected(Tile t) {
+		if (path.isEmpty()
+				|| path.get(path.size() - 1).getAdjascent().contains(t)) {
+			path.add(t);
+			t.setState(State.DOWN);
+			setChanged();
+			notifyObservers();
+		}
+	}
 
+	/**
+	 * Returns the currently selected path
+	 */
+	public ArrayList<Tile> getPath() {
+		return path;
+	}
 
-    /**
-     * If the tile is valid, (based on the last selected tile) the tile is added
-     * to the current path list and has its state changed to DOWN
-     */
-    public void setSelected(int x, int y)
-    {
-        /* if( is valid tile) */
-        setChanged();
-        notifyObservers();
-        // TODO
-    }
+	/**
+	 * Called when tile selection is done and the word is submitted. Uses
+	 * WordSolver to check if the word contained in the current path is valid
+	 * Sets the tiles in the path as good or bad depending on whether the word
+	 * was good or bad
+	 * 
+	 * @precondition the contained in the current path is valid
+	 */
+	public void submitWord() {
+		String s = "";
+		for (Tile t : path) {
+			s += t.getLetter();
+		}
+		if (WordSolver.getInstance().isWord(s)) {
+			for (Tile t : path) {
+				t.setState(State.GOOD);
+			}
+		} else {
+			for (Tile t : path) {
+				t.setState(State.BAD);
+			}
+		}
+	}
 
+	public Tile getTile(int x, int y) {
+		return grid[x][y];
+	}
 
-    /**
-     * Returns the currently selected path
-     */
-    public ArrayList<Tile> getPath()
-    {
-        // TODO
-        return null;
-    }
+	/**
+	 * Clears the current path, sets all tiles in the path to their default
+	 * color
+	 */
+	public void deselectPath() {
+		path.clear();
+	}
 
-
-    /**
-     * Called when tile selection is done and the word is submitted. Uses
-     * WordSolver to check if the word contained in the current path is valid
-     * Sets the tiles in the path as good or bad depending on whether the word
-     * was good or bad
-     *
-     * @precondition the contained in the current path is valid
-     */
-    public void submitWord()
-    {
-        // TODO
-    }
-
-
-    public Tile getTile(int x, int y)
-    {
-        return grid[x][y];
-    }
-
-
-    /**
-     * Clears the current path, sets all tiles in the path to their default
-     * color
-     */
-    public void deselectPath()
-    {
-        // TODO
-    }
-
-
-    /**
-     * Returns the side length of the square grid
-     */
-    public int size()
-    {
-        return grid.length;
-    }
+	/**
+	 * Returns the side length of the square grid
+	 */
+	public int size() {
+		return grid.length;
+	}
 
 }
--- src/com/WordFinder/Tests/LetterGridTest.java
+++ src/com/WordFinder/Tests/LetterGridTest.java
@@ -2,61 +2,76 @@ package com.WordFinder.Tests;
 
 import com.WordFinder.LetterGrid;
 import com.WordFinder.Tile;
+import com.WordFinder.Tile.State;
+
 import java.util.ArrayList;
 import student.TestCase;
 
 public class LetterGridTest extends TestCase {
 
-    private LetterGrid grid;
+	private LetterGrid grid;
 
-    public void setUp() {
-	grid = new LetterGrid();
-    }
-
-    public void testLoadRandom() {
-	grid.loadRandom(4);
-	assertEquals(grid.size(), 4);
-	for (int i = 0; i < grid.size(); i++) {
-	    for (int j = 0; j < grid.size(); j++) {
-		char c = (char) (grid.getTile(i, j).getLetter() - 'a');
-		assertTrue(c >= 0 && c <= 26); // make sure all letters are in
-					       // [a,z]
-	    }
+	public void setUp() {
+		grid = new LetterGrid();
 	}
-    }
 
-    public void testLoad() {
-	grid.load(generateGrid("abc","def","ghi"));
-	assertTilesString("abcdfghi",grid.getTile(1,1).getAdjascent());
-    }
-
-    public void testSetSelected() {
-	grid.load(generateGrid("ab", "cd"));
-	grid.setSelected(0, 0);
-	grid.setSelected(1, 0);
-	grid.setSelected(0, 1);
-	assertTilesString("abc",grid.getPath());
-
-    }
-
-    public void testSubmitWord() {
-	//TODO
-    }
-
-    private void assertTilesString(String string,ArrayList<Tile> path) {
-	assertNotNull(path);
-	String  s = "";
-	for (int i = 0; i < path.size(); i++) {
-	    s+=path.get(i).getLetter();
+	public void testLoadRandom() {
+		grid.loadRandom(4);
+		assertEquals(grid.size(), 4);
+		for (int i = 0; i < grid.size(); i++) {
+			for (int j = 0; j < grid.size(); j++) {
+				char c = (char) (grid.getTile(i, j).getLetter() - 'a');
+				assertTrue(c >= 0 && c <= 26); // make sure all letters are in
+				// [a,z]
+			}
+		}
 	}
-	assertEquals(string,s);
-    }
 
-    private char[][] generateGrid(String... strings) {
-	char[][] letters = new char[strings.length][strings.length];
-	for (int i = 0; i < strings.length; i++) {
-	    letters[i] = strings[i].toCharArray();
+	public void testLoad() {
+		grid.load(generateGrid("abc", "def", "ghi"));
+		assertTilesString("abcdfghi", grid.getTile(1, 1).getAdjascent());
 	}
-	return letters;
-    }
+
+	public void testSetSelected() {
+		grid.load(generateGrid("ab", "cd"));
+		grid.setSelected(grid.getTile(0, 0));
+		grid.setSelected(grid.getTile(1, 0));
+		grid.setSelected(grid.getTile(0, 1));
+		assertEquals(grid.getTile(0, 0).getState(), State.DOWN);
+		assertEquals(grid.getTile(1, 0).getState(), State.DOWN);
+		assertEquals(grid.getTile(0, 1).getState(), State.DOWN);
+		assertEquals(grid.getTile(1, 1).getState(), State.UP);
+		assertTilesString("abc", grid.getPath());
+
+	}
+
+	public void testSubmitWord() {
+		// TODO
+	}
+	
+	public void testGetTile(){
+		grid.load(generateGrid("ab","cd"));
+		assertEquals(grid.getTile(1, 0).getX(),1);
+		assertEquals(grid.getTile(1, 0).getLetter(),'b');
+		assertEquals(grid.getTile(1, 0).getY(),0);
+		assertEquals(grid.getTile(0, 1).getX(),0);
+		assertEquals(grid.getTile(0, 1).getY(),1);
+	}
+
+	private void assertTilesString(String string, ArrayList<Tile> path) {
+		assertNotNull(path);
+		String s = "";
+		for (int i = 0; i < path.size(); i++) {
+			s += path.get(i).getLetter();
+		}
+		assertEquals(string, s);
+	}
+
+	private char[][] generateGrid(String... strings) {
+		char[][] letters = new char[strings.length][strings.length];
+		for (int i = 0; i < strings.length; i++) {
+			letters[i] = strings[i].toCharArray();
+		}
+		return letters;
+	}
 }
