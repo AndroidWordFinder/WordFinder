cc40d7dc11a6e6ed822763d9c97c70b74b74e594
Christopher Buck <cmbuck@vt.edu>
2011-12-01 22:24:22 -0500

    Added TitleScreenActivity and title.xml

---
+++ src/com/WordFinder/TitleScreenActivity.java
@@ -0,0 +1,52 @@
+package com.WordFinder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ *  The activity for the Title Screen of the WordFinder application
+ *
+ *  @author cmbuck
+ *  @version Dec 1, 2011
+ */
+public class TitleScreenActivity extends Activity
+{
+    //GUI elements
+    TextView titleTextView;
+    Button start4x4;
+    Button start5x5;
+    Button solo;
+    //Button Dummy
+    Button about;
+    Button instructions;
+
+    /**
+     * Creates the TitleScreenActivity
+     * @param savedInstanceState The saved instance state
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        //set GUI element references
+        titleTextView = (TextView)findViewById(R.id.title);
+        start4x4 = (Button)findViewById(R.id.start4x4Button);
+        start5x5 = (Button)findViewById(R.id.start5x5Button);
+        solo = (Button)findViewById(R.id.soloButton);
+        about = (Button)findViewById(R.id.aboutButton);
+        instructions = (Button)findViewById(R.id.instructionsButton);
+
+        setContentView(R.layout.results);
+    }
+
+    /**
+     * Loads the dictionary of words
+     */
+    public void loadDictionary()
+    {
+        //TODO: load the dictionary
+    }
+}
