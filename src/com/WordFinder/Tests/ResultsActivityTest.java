90b8357962bcdb28a1398acb95b905402b4b3367
Christopher Buck <cmbuck@vt.edu>
2011-12-04 20:51:17 -0500

    Added ResultsActivityTest and tests

---
+++ src/com/WordFinder/Tests/ResultsActivityTest.java
@@ -0,0 +1,59 @@
+package com.WordFinder.Tests;
+
+import android.widget.Button;
+import com.WordFinder.R;
+import com.WordFinder.ResultsActivity;
+
+public class ResultsActivityTest
+    extends student.AndroidTestCase<ResultsActivity>
+{
+
+    private ResultsActivity activity;
+
+    /**
+     * Constructs a new ResultsActivityTest
+     */
+    public ResultsActivityTest()
+    {
+        super(ResultsActivity.class);
+    }
+
+    /**
+     * Sets up the testing environment
+     */
+    public void setUp()
+    {
+        activity = getActivity();
+        String [] userWords = {"dog", "cat", "hat"};
+        String [] solvedWords = {"dog", "cat", "rat", "hat", "mat", "town"};
+        activity.getIntent().putExtra("userWords", userWords);
+        activity.getIntent().putExtra("solvedWords", solvedWords);
+    }
+
+    /**
+     * Tests the onPause() and onResume() methods
+     */
+    public void testOnPauseAndOnResume()
+    {
+        activity.onPause();
+        activity.onResume();
+    }
+
+    /**
+     * Tests the onTitleScreenClicked() method
+     */
+    public void testOnTitleScreenClicked()
+    {
+        click(getView(Button.class, R.id.titleScreenButton));
+    }
+
+    /**
+     * Tests the onPlayAgainClicked() method
+     */
+    public void testOnPlayAgainClicked()
+    {
+        click(getView(Button.class, R.id.playAgainButton));
+    }
+
+
+}
